15a7e88380de2430cadd305966c5be8fbbce877b
Arsteinberg <arsteinberg93@gmail.com>
2019-07-08 19:24:01 -0400

    need to figure out how to connect all the files

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,20 @@
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+
+
+
+    app.post("/api/friends", function(req, res) {
+
+        if (friends.length < 5) {
+            friends.push(req.body);
+            res.json(true);
+        }
+
+    });
+
+};
