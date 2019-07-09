2e15f95ff5d9da31808ea69ce9ef84b86d0b2b34
Arsteinberg <arsteinberg93@gmail.com>
2019-07-09 11:24:53 -0400

    fixed routes and connected homepage to survey

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -9,15 +10,16 @@ var express = require("express");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 
 
 app.listen(PORT, function() {
